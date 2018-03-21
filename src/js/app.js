67699c9a156e5024c431754ce9bc6568aaec83c4
POKEMOS64 <polevoy.sergey@hotmail.com>
2018-03-21 18:16:16 +0400

    start

---
+++ src/js/app.js
@@ -0,0 +1,59 @@
+$('.navigation').on('click','a', function(event){
+	event.preventDefault();
+	
+	var id = $(this).attr("href"),
+			top = $(id).offset().top;
+	$('body,html').animate({scrollTop: top},1500);
+});
+(function($) {
+$(function() {
+$('ul.catalog__container_tabs').on('click', 'li:not(.active)', function(){
+    $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.catalog__container').find('div.catalog__container_tab').removeClass('active').eq($(this).index()).addClass('active');
+    });
+ 
+});
+})(jQuery);
+$('#navi_open').click(function(){
+    $('.header__menu, #navi_close').fadeIn(400); 
+    $('#navi_open').fadeOut(400); 
+  });
+$(' #navi_close').click(function(){
+			$('.header__menu, #navi_open').fadeOut(400); 
+			$('#navi_close').fadeOut(400); 
+	$('#navi_open').fadeIn(400);
+		});
+
+$('.slide').owlCarousel({
+		margin:0,
+		nav:false,
+		startPosition: 'URLHash',
+		loop: true,
+		center: true,
+		items:1,
+		dots:true,
+		autoplay:3000,
+		dotsContainer: '#slide-cl__id',
+});
+
+
+$('.portfolio-box,.patron-box').owlCarousel({
+		loop:true,
+    margin:5,
+    nav:false,
+		autoplay:true,
+		autoplayTimeout:5000,
+		autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
